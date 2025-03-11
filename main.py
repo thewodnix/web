4ad0bea46602999ba371007e685bab3635c4d4b7
wdnx <ftsu29@icloud.com>
2025-03-11 15:36:02 +0300

    gf

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from data import db_session
+from data.users import User
+from data.news import News
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    user = User()
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.id == 1).first()
+    news = News(title="Личная запись", content="Эта запись личная",
+                is_private=True)
+    user.news.append(news)
+    db_sess.commit()
+    #app.run()
+
+
+if __name__ == '__main__':
+    main()
